0099c3320b6fe1d1cbc9e6015df6f3df08ae9b0b
Thiago Porciúncula <tfcporciuncula@gmail.com>
2015-09-20 21:44:27 -0300

    Adding Thymeleaf; POM refactor.

--- src/main/java/xyz/jhow/Jhow.java
+++ src/main/java/xyz/jhow/Jhow.java
@@ -1,16 +1,23 @@
 package xyz.jhow;
 
+import spark.ModelAndView;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
+
+import java.util.HashMap;
+
 import static spark.Spark.*;
 
 public class Jhow {
 
+    private static final String STATIC_FILES_FOLDER = "/static";
     private static final String PORT_ENV_VARIABLE_NAME = "PORT";
     private static final int DEFAULT_PORT = 8080;
 
     public static void main(String args[]) {
         setPortFromEnvVariable();
+        staticFileLocation(STATIC_FILES_FOLDER);
 
-        get("*", (req, res) -> "Hi.");
+        get("*", (req, res) -> new ModelAndView(new HashMap(), "index"), new ThymeleafTemplateEngine());
     }
 
     private static void setPortFromEnvVariable() {
